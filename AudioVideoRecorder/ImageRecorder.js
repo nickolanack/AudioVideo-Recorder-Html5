00a5d2881e86e4311637ebf41d0a0ebc989a66cd
Nick Blackwell <nickblackwell82@gmail.com>
2014-11-10 14:36:16 -0800

    moved useful code from ImageRecorderModule to ImageRecorder class. (module should just be the user interface)

--- AudioVideoRecorder/ImageRecorder.js
+++ AudioVideoRecorder/ImageRecorder.js
@@ -1,3 +1,43 @@
+/**
+ * ImageRecorder, and CameraRecorder 
+ * 
+ * 
+ * Example:
+ * 
+ * 		var recorder;
+ * 		(recorder=new CameraRecorder()).addEvent('onCreatedRecorder',function(){
+			
+			var video=recorder.getVideoElement();
+			//do something with video el... put it into the document...
+			
+			//make a button. 'take photo' button
+			var button; 
+			// ...
+			// ...
+			button.addEvent('click', function(value){
+				
+					//take a picture. retrieve as url
+					var i=new Asset.image(recorder.getImageUrl('image/png'));
+					//do something with image. put it in document. 
+					
+					// or get image blob
+					
+					var b=recorder.getImageBlob('image/png');
+					//could upload to server
+					
+			});
+
+				
+				
+		});			
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+
 var ImageRecorder = new Class({
 	Implements:Events,
 	initialize:function(){},
@@ -24,7 +64,7 @@ var ImageRecorder = new Class({
 		var ctx = canvas.getContext('2d');
 		ctx.drawImage(me.video, 0, 0);
 		ctx.scale(-1,1); //mirror on x. seems more natural
-		if((['image/png', 'img/jpeg', 'img/webp']).indexOf(format)>=0){
+		if((['image/png', 'image/jpeg', 'image/webp']).indexOf(format)>=0){
 			
 			return canvas.toDataURL(format, (quality>0)?quality:1);
 
@@ -59,7 +99,9 @@ var ImageRecorder = new Class({
 
 
 CameraRecorder=new Class({
+	
 	Extends:ImageRecorder,
+	//options aren't used
 	initialize:function(options){
 		var me=this;
 	
